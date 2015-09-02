f4318b1bf56cf4b3181f346f4450ed9dd44037f2
Derwin McGeary <derwinmcgeary+web@gmail.com>
2015-09-02 20:58:23 +0100

    now automatically downloading all images to images/ and fixing the src attributes in hwcode.html

--- rebuildcode.sh
+++ rebuildcode.sh
@@ -2,3 +2,5 @@
 rm hwcode.html downloadedhwcode.html
 ./hwcodedownloader.sh
 cat htmlpreamble.html downloadedhwcode.html htmlcoda.html | awk -f duplicateremover.awk  > hwcode.html
+cat hwcode.html | grep src | sed -e 's/src="//g' -e 's/"$//g' | wget -i - -P images
+sed -i -e 's/https:\/\/assets.digital.cabinet-office.gov.uk\/static\/hc/images/g' hwcode.html
