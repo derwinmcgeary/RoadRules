00bbc424a53c5c31d86cff34a4dd1491d439f53e
Derwin McGeary <derwinmcgeary+web@gmail.com>
2015-09-03 15:06:40 +0100

    all is well. continue coding. all is well.

--- rebuildcode.sh
+++ rebuildcode.sh
@@ -2,5 +2,6 @@
 rm hwcode.html downloadedhwcode.html
 ./hwcodedownloader.sh
 cat htmlpreamble.html downloadedhwcode.html htmlcoda.html | awk -f duplicateremover.awk  > hwcode.html
-cat hwcode.html | grep src | sed -e 's/src="//g' -e 's/"$//g' | wget -i - -P images
-sed -i -e 's/https:\/\/assets.digital.cabinet-office.gov.uk\/static\/hc/images/g' hwcode.html
+cat hwcode.html | grep src | sed -e 's/src="//g' -e 's/"$//g' | wget -nc -i - -P images
+mv hc_rule_252* rule_252.jpg
+sed -i -e 's/https:\/\/assets.digital.cabinet-office.gov.uk\/static\/hc\///g' -e 's/rule_252.*jpg/rule_252.jpg/g' hwcode.html
