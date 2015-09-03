ca7bddf8f9bd3c59575b433b86bbcd5f62efa24c
Derwin McGeary <derwinmcgeary+web@gmail.com>
2015-09-03 17:04:58 +0100

    weird braino on that annyoning (hc_)rule_252.jpg

--- rebuildcode.sh
+++ rebuildcode.sh
@@ -4,7 +4,7 @@ rm hwcode/hwcode.html downloadedhwcode.html
 cat htmlpreamble.html downloadedhwcode.html htmlcoda.html | awk -f duplicateremover.awk  > hwcode.html
 cat hwcode.html | grep src | sed -e 's/src="//g' -e 's/"$//g' | wget -nc -i - -P hwcode
 # rule 252 has too long a filename for android, apparently, so...
-mv hwcode/hc_rule_252* hwcode/rule_252.jpg
+mv hwcode/hc_rule_252* hwcode/hc_rule_252.jpg
 sed -i -e 's/https:\/\/assets.digital.cabinet-office.gov.uk\/static\/hc\///g' -e 's/rule_252.*jpg/rule_252.jpg/g' hwcode.html
 sed -i -e 's/height=".*"//g' -e 's/width=".*"/width="100%"/g' hwcode.html
 mv hwcode.html hwcode/
