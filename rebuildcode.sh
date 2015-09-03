c78c09b2117374ede7f430ba391ad54a60f3ba15
Derwin McGeary <derwinmcgeary+web@gmail.com>
2015-09-03 15:47:30 +0100

    for convenience, everything is ready-to-use now in hwcode/ : just copy!

--- rebuildcode.sh
+++ rebuildcode.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-rm hwcode.html downloadedhwcode.html
+rm hwcode/hwcode.html downloadedhwcode.html
 ./hwcodedownloader.sh
 cat htmlpreamble.html downloadedhwcode.html htmlcoda.html | awk -f duplicateremover.awk  > hwcode.html
-cat hwcode.html | grep src | sed -e 's/src="//g' -e 's/"$//g' | wget -nc -i - -P images
-mv hc_rule_252* rule_252.jpg
+cat hwcode.html | grep src | sed -e 's/src="//g' -e 's/"$//g' | wget -nc -i - -P hwcode
+# rule 252 has too long a filename for android, apparently, so...
+mv hwcode/hc_rule_252* hwcode/rule_252.jpg
 sed -i -e 's/https:\/\/assets.digital.cabinet-office.gov.uk\/static\/hc\///g' -e 's/rule_252.*jpg/rule_252.jpg/g' hwcode.html
+mv hwcode.html hwcode/
