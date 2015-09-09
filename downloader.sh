c51120deed225e5aae7a8f51e3cf097b505a9d57
Derwin McGeary <derwinmcgeary+web@gmail.com>
2015-09-09 20:23:13 +0100

    make introduction more properer htmlwise

--- downloader.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-wget --wait 3 --random-wait -i urlslist.txt -O - | tr "\n" "|" | grep -o '<\/aside>.*<footer>' | sed 's/\(<\/aside>\|<footer>\)//g' | sed 's/|/\n/g' | xml2asc > downloaded.html
